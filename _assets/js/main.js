769c46464ca8f71fa7bf5d98be6daa9baa20a2cb
Mike Riley <mikeriley131@gmail.com>
2017-03-10 14:10:23 -0700

    February updates

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  //FAQ Accordion
   var allPanels = $('.faq__list > .faq__definition').hide();
 
   $('.faq__term').css({'margin-bottom': '1em'});
@@ -8,6 +9,31 @@ $(document).ready(function() {
     allPanels.slideUp();
     $(this).parent().next().slideDown();
     return false;
+  });
+
+  //For Midwives Sorting
+  $('.js-filter__ga').on('click', function() {
+    $('.general-announcement').show('fast');
+    $('article').not('.general-announcement').hide('fast');
+  });
+
+  $('.js-filter__lu').on('click', function() {
+    $('.leg-update').show('fast');
+    $('article').not('.leg-update').hide('fast');
+  });
+
+  $('.js-filter__mm').on('click', function() {
+    $('.meeting-mins').show('fast');
+    $('article').not('.meeting-mins').hide('fast');
+  });
+
+  $('.js-filter__jp').on('click', function() {
+    $('.job-post').show('fast');
+    $('article').not('.job-post').hide('fast');
+  });
+
+  $('.js-filter__all').on('click', function() {
+    $('.news-grouping').show('fast');
   });
 
 });
